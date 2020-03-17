6b3e3057c69b354dbab058486794fd39cd3c44a9
Benjamin Brier <ben.brier14@gmail.com>
2020-03-17 19:08:16 -0400

    model parameters, get user media, selected things from html done

---
+++ app.js
@@ -0,0 +1,16 @@
+const modelParams = {
+	flipHorizontal: true, // flip e.g for video
+	imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+	maxNumBoxes: 1, // maximum number of boxes to detect
+	iouThreshold: 0.5, // ioU threshold for non-max suppression
+	scoreThreshold: 0.89 // confidence threshold for predictions.
+};
+
+navigator.getUserMedia =
+	navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+//SELECT EVERYTYHING FROM HTMl
+
+const video = document.querySelector('#video');
+const audio = document.querySelector('#audio');
+let model;
