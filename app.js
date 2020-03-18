0fb06ccc06a1a399ae84301dc4488c85dc5fd005
Benjamin Brier <ben.brier14@gmail.com>
2020-03-18 15:48:35 -0400

    run detection if statements work

--- app.js
+++ app.js
@@ -17,16 +17,38 @@ let model;
 
 handTrack.startVideo(video).then((status) => {
 	if (status) {
-		navigator.getUserMedia({ video: {} }, (stream) => {
-			video.srcObject = stream;
-			//Run Out Detection
-		});
+		navigator.getUserMedia(
+			{ video: {} },
+			(stream) => {
+				video.srcObject = stream;
+				//Run Out Detection
+				setInterval(runDetection, 100);
+			},
+			(err) => console.log(err)
+		);
 	}
 });
 
 function runDetection() {
 	model.detect(video).then((predictions) => {
-		console.log(predictions);
+		if (predictions.length !== 0) {
+			let hand1 = predictions[0].bbox;
+			let x = hand1[0];
+			let y = hand1[1];
+
+			if (y > 300) {
+				if (x < 200) {
+					audio.src = 'a-chord.mp3';
+				} else if (x > 400) {
+					audio.src = 'e-chord.mp3';
+				} else if (x > 300) {
+					audio.src = 'c-chord.mp3';
+				} else if (x > 200) {
+					audio.src = 'b-chord.mp3';
+				}
+			}
+			audio.play();
+		}
 	});
 }
 
